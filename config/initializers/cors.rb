2d1598ede3d32447cada24489ee81e5b0fef8e83
carlos <cmramseyer@gmail.com>
2026-09-07 23:30:39 -0300

    cors origin in env var

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,21 @@
-frontend_origin = Rails.application.credentials.cors_origins.to_s
-unless frontend_origin.match?(%r{\Ahttps?://[^/]+\z}) && frontend_origin != "*"
-  raise "cors_origins must be one exact HTTP(S) origin"
+configured_origins = ENV.fetch("CORS_ORIGINS", Rails.application.credentials.cors_origins.to_s)
+cors_origins = configured_origins.split(",").map(&:strip).reject(&:empty?)
+
+valid_origin = lambda do |origin|
+  uri = URI.parse(origin)
+  uri.is_a?(URI::HTTP) && uri.host.present? && uri.path.empty? && uri.query.nil? &&
+    uri.fragment.nil? && uri.userinfo.nil? && !uri.host.include?("*")
+rescue URI::InvalidURIError
+  false
+end
+
+unless cors_origins.any? && cors_origins.all?(&valid_origin)
+  raise "CORS_ORIGINS must contain one or more exact HTTP(S) origins"
 end
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins frontend_origin
+    origins(*cors_origins)
 
     resource "*",
       headers: :any,
